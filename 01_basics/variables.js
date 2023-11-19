3a2002655562384635773e4cb52253fcf86d39f1
DarshanMD04 <123662714+DarshanMD04@users.noreply.github.com>
2023-11-19 05:45:39 +0000

    variable.ok

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 34343434
+let accountemail = "darshan@gail.com"
+let accountpassword = "92849832748"
+let hii;
+
+console.log(accountId);
+
+
+console.table([accountId,accountemail,accountpassword,hii])
