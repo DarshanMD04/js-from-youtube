1e135087601cc41ad382629125edde68ec9853b1
DarshanMD04 <123662714+DarshanMD04@users.noreply.github.com>
2023-11-21 16:15:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,52 @@
+/*let onename = "true"
+console.log(typeof onename);
+
+let anothernumber = Number(onename)
+console.log(typeof anothernumber );
+console.log(anothernumber);
+//NaN means =>
+*/
+
+
+/*let isLoggedIn = 'hii'
+
+let isLoggedIn2 = Boolean(isLoggedIn)
+console.log(isLoggedIn2);
+
+let oneNumber = 44
+
+let otherNumber = String(oneNumber)
+console.log(otherNumber);
+console.log(typeof otherNumber);
+*/
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/ //*********************** Operations ***********************
